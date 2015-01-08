13295094d1578b8088afeefaa069e130ac13179e
Brad Davis <bradrobertdavis@gmail.com>
2015-01-08 12:17:38 -0800

    Adding property to sidenav obj that highlights navigation items on page load

--- app/js/sideNav/controllers/sideNav.controller.js
+++ app/js/sideNav/controllers/sideNav.controller.js
@@ -3,16 +3,9 @@ htsApp.controller('sideNav.controller', ['$scope', '$rootScope', 'sideNavFactory
     $scope.sideNav = sideNavFactory;
 
     //TODO: SideNav does not correctly highlight menu item on page load.
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
 
-    //$scope.selected = {
-    //    $$hashKey: "object:14",
-    //    alerts : null,
-    //    link : 'settings.general',
-    //    name : 'General Settings'
-    //};
+        sideNavFactory.settingsMenu[0].link = fromState.name;
+    });
 
-    //sideNavFactory.watchme = {
-    //    scope : $scope,
-    //    prent : $rootScope
-    //};
 }]);
--- app/js/sideNav/factories/sideNav.factory.js
+++ app/js/sideNav/factories/sideNav.factory.js
@@ -1,29 +1,31 @@
 /**
  * Created by braddavis on 11/29/14.
  */
-htsApp.factory('sideNavFactory', ['Session', function (Session) {
+htsApp.factory('sideNavFactory', ['Session', 'forSaleFactory', function (Session, forSaleFactory) {
 
     var factory = {};
 
-    console.log(Session.userObj);
-
     //DEFAULT MENU
     factory.defaultMenu = [{
-        name: "My Feeds",
+        name: "My Feed",
         alerts: null,
-        link: "feed"
+        link: "feed",
+        active: false
     }, {
         name: "I'm Selling",
         alerts: null,
-        link: "selling"
+        link: "selling",
+        active: false
     }, {
         name: "I'm Interested",
         alerts: Session.userObj.user_settings.favorites.length,
-        link: "interested"
+        link: "interested",
+        active: false
     }, {
         name: "Notifications",
         alerts: null,
-        link: "notifications"
+        link: "notifications",
+        active: false
     }];
 
 
@@ -34,15 +36,18 @@ htsApp.factory('sideNavFactory', ['Session', function (Session) {
     factory.splashMenu = [{
         name: "Make an Offer",
         alerts: null,
-        link: "selling"
+        link: "selling",
+        active: false
     }, {
         name: "I'm Interested",
         alerts: null,
-        link: "interested"
+        link: "interested",
+        active: false
     }, {
         name: "Mark as Spam",
         alerts: null,
-        link: "notifications"
+        link: "notifications",
+        active: false
     }];
 
 
@@ -50,44 +55,131 @@ htsApp.factory('sideNavFactory', ['Session', function (Session) {
 
     //SETTINGS MENU
     factory.settingsMenu = [{
+        name: "Back",
+        alerts: null,
+        link: null,
+        active: false
+    }, {
         name: "General Settings",
         alerts: null,
-        link: "settings.general"
+        link: "settings.general",
+        active: false
     }, {
         name: "Edit Profile",
         alerts: null,
-        link: "settings.profile"
+        link: "settings.profile",
+        active: false
     }, {
         name: "Payment Settings",
         alerts: null,
-        link: "settings.payment"
+        link: "settings.payment",
+        active: false
     }];
 
 
     //This function called by ui-router as moves through application.  Updates choice in side nav dynamically.
     factory.updateSideNav = function (toState) {
         //
-        //console.log(toState.name);
+        //alert(toState.name);
         //
         //console.log('here is watch me', factory.watchme);
 
         switch (toState.name) {
+            //Settings Menu
             case 'settings':
-            case 'settings.general':
-            case 'settings.profile':
-            case 'settings.payment':
+                factory.settingsMenu[0].active = false;
+                factory.settingsMenu[1].active = false;
+                factory.settingsMenu[2].active = false;
+                factory.settingsMenu[3].active = false;
                 factory.items = factory.settingsMenu;
                 break;
+            case 'settings.general':
+                factory.settingsMenu[0].active = false;
+                factory.settingsMenu[1].active = true;
+                factory.settingsMenu[2].active = false;
+                factory.settingsMenu[3].active = false;
+                factory.items = factory.settingsMenu;
+                break;
+            case 'settings.profile':
+                factory.settingsMenu[0].active = false;
+                factory.settingsMenu[1].active = false;
+                factory.settingsMenu[2].active = true;
+                factory.settingsMenu[3].active = false;
+                factory.items = factory.settingsMenu;
+                break;
+            case 'settings.payment':
+                factory.settingsMenu[0].active = false;
+                factory.settingsMenu[1].active = false;
+                factory.settingsMenu[2].active = false;
+                factory.settingsMenu[3].active = true;
+                factory.items = factory.settingsMenu;
+                break;
+
+
+
+            //Splash Menu
             case 'results.splash':
-            case 'interested.splash':
+                factory.splashMenu[0].active = false;
+                factory.splashMenu[1].active = false;
+                factory.splashMenu[2].active = false;
                 factory.items = factory.splashMenu;
                 break;
+            case 'interested.splash':
+                factory.splashMenu[0].active = false;
+                factory.splashMenu[1].active = false;
+                factory.splashMenu[2].active = false;
+                factory.items = factory.splashMenu;
+                break;
+            case 'selling.splash':
+                factory.splashMenu[0].active = false;
+                factory.splashMenu[1].active = false;
+                factory.splashMenu[2].active = false;
+                factory.items = factory.splashMenu;
+                break;
+
+
+
+            //Default Menu
             case 'feed':
+                factory.defaultMenu[0].active = true;
+                factory.defaultMenu[1].active = false;
+                factory.defaultMenu[2].active = false;
+                factory.defaultMenu[3].active = false;
+                factory.items = factory.defaultMenu;
+                break;
             case 'selling':
+                factory.defaultMenu[0].active = false;
+                factory.defaultMenu[1].active = true;
+                factory.defaultMenu[2].active = false;
+                factory.defaultMenu[3].active = false;
+                factory.items = factory.defaultMenu;
+                break;
             case 'interested':
+                factory.defaultMenu[0].active = false;
+                factory.defaultMenu[1].active = false;
+                factory.defaultMenu[2].active = true;
+                factory.defaultMenu[3].active = false;
+                factory.items = factory.defaultMenu;
+                break;
             case 'notifications':
+                factory.defaultMenu[0].active = false;
+                factory.defaultMenu[1].active = false;
+                factory.defaultMenu[2].active = false;
+                factory.defaultMenu[3].active = true;
+                factory.items = factory.defaultMenu;
+                break;
             case 'results':
+                factory.defaultMenu[0].active = false;
+                factory.defaultMenu[1].active = false;
+                factory.defaultMenu[2].active = false;
+                factory.defaultMenu[3].active = false;
+                factory.items = factory.defaultMenu;
+                break;
             case 'profile':
+                factory.defaultMenu[0].active = false;
+                factory.defaultMenu[1].active = false;
+                factory.defaultMenu[2].active = false;
+                factory.defaultMenu[3].active = false;
                 factory.items = factory.defaultMenu;
                 break;
         }
