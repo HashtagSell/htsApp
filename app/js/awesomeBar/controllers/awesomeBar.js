a5c62f3815c4918211942718f3549f9e1487e587
Brad Davis <bradrobertdavis@gmail.com>
2015-01-11 17:28:10 -0800

    Beginning to add autocomplete to search bar

--- app/js/awesomeBar/controllers/awesomeBar.js
+++ app/js/awesomeBar/controllers/awesomeBar.js
@@ -1,98 +1,95 @@
-htsApp.controller('awesomeBarController', ['$window', '$scope', '$location', function ($window, $scope, $location) {
-
-
-//    //This function will add the appropirate HTML into any div with the class .awesomeBar
-//    $scope.lookupLocation = function () {
-//
-//        $scope.loading = true;
-//
-//        $scope.placeholder = UpdatePlaceholder;
-//
-//        var service = $window.location.protocol + '//freegeoip.net/json/';
-//
-//        var firstAttempt = geolocationService.locateUser(service);
-//
-//        firstAttempt.then(function (response) {
-//
-//            $scope.userLocationObject = response;
-//            $scope.loading = false;
-//
-//        }, function (reason) {
-//
-//            console.log("trying backup service");
-//
-//            var secondService = $window.location.protocol + '//www.telize.com/geoip';
-//
-//            var secondAttempt = geolocationService.locateUser(secondService);
-//
-//            secondAttempt.then(function (response) {
-//
-//                $scope.userLocationObject = response;
-//                $scope.loading = false;
-//
-//            }, function (reason) {
-//
-//                console.log("Falling back to manual city search");
-//
-//                $scope.userLocationObject = {};
-//
-//                $scope.loading = false;
-//
-//                $scope.userLocationObject.geoLocationSuccess = false;
-//                $scope.userLocationObject.manualCitySearch = {};
-//                $scope.userLocationObject.manualCitySearch.request = true;
-//
-//            });
-//
-//        });
-//
-//    }
-//
-//    $scope.lookupLocation();
+htsApp.controller('awesomeBarController', ['$window', '$scope', '$location', 'newPostFactory', function ($window, $scope, $location, mentionsFactory) {
 
     //Redirects to results page with correct params
     $scope.awesomeBarSubmit = function () {
         $location.path("/results/"+$scope.query+"/");
-//        if ($scope.userLocationObject.manualCitySearch.lat && $scope.userLocationObject.manualCitySearch.lon) {
-//            console.log($scope.userLocationObject.manualCitySearch);
-//            console.log("redirecting");
-//            var redirectUrl = "/results?q=" + $scope.userLocationObject.manualCitySearch.query + "&city=" + $scope.userLocationObject.manualCitySearch.cityCommaState + "&lat=" + $scope.userLocationObject.manualCitySearch.lat + "&lon=" + $scope.userLocationObject.manualCitySearch.lon;
-//            console.log(redirectUrl);
-//            $window.location.href = redirectUrl;
-//        } else {
-//
-//        }
+    };
+
+    //========= # Products =========
+    $scope.searchProducts = function (term) {
+        if (term) {
+            mentionsFactory.predictProduct(term).then(function (results) {
+                $scope.products = results;
+                console.log("Here is scope.products", $scope.products);
+            });
+        }
+    };
+
+    $scope.getProductTextRaw = function (product) {
+        mentionsFactory.getProductMetaData(product).then(function (jsonTemplate) {
+            console.log(jsonTemplate);
+            console.log("done");
+        });
+        //return '<span class="mention-highlighter" contentEditable="false">#' + product.value + '</span>';
+        return product.value;
+    };
+
+
+    //========= @ Places =========
+    $scope.map = mentionsFactory.googleMap;
+
+    $scope.searchPlaces = function (term) {
+        if (term) {
+            mentionsFactory.predictPlace(term).then(function (results) {
+                $scope.places = results;
+                console.log("Here is scope.places", $scope.places);
+            });
+        }
+    };
+
+    $scope.getPlacesTextRaw = function (selectedPlace) {
+        mentionsFactory.getPlaceMetaData(selectedPlace).then(function (jsonTemplate) {
+            console.log(jsonTemplate);
+            console.log("done");
+        });
+        console.log("updated ui");
+        //return '<span class="mention-highlighter" contentEditable="false">@' + selectedPlace.description + '</span>';
+        return '@' + selectedPlace.description;
+    };
+
+    //========= $ Prices =========
+    $scope.searchPrice = function (term) {
+        if (term) {
+            $scope.prices = mentionsFactory.predictPrice(term);
+            console.log("here is scope.prices", $scope.prices);
+        }
+    };
+
+    $scope.getPricesTextRaw = function (selectedPrice) {
+        mentionsFactory.getPriceMetaData(selectedPrice);
+        //return '<span class="mention-highlighter-price" contentEditable="false">$' + selectedPrice.suggestion + '</span>';
+        return '$' + selectedPrice.suggestion;
     };
 
 }]);
 
 
-htsApp.directive('googleplace', function () {
-    return {
-        require: 'ngModel',
-        link: function (scope, element, attrs, model) {
-            var options = {
-                types: ['(cities)'],
-                componentRestrictions: {country: 'us'}
-            };
-            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
-
-            google.maps.event.addListener(scope.gPlace, 'place_changed', function () {
-
-                var latlongString = this.getPlace().geometry.location.toString();
-                var cleanlatlongString = latlongString.substring(1, latlongString.length - 1);
-                var latlonArray = cleanlatlongString.split(',');
-
-                scope.userLocationObject.manualCitySearch.lat = latlonArray[0];
-                scope.userLocationObject.manualCitySearch.lon = latlonArray[1].substring(1);
-                scope.userLocationObject.manualCitySearch.cityCommaState = this.getPlace().formatted_address;
-
-                scope.$apply(function () {
-                    model.$setViewValue(element.val());
-                });
-
-                scope.awesomeBarSubmit();
-            });
-        }
-    };
-});
+//htsApp.directive('googleplace', function () {
+//    return {
+//        require: 'ngModel',
+//        link: function (scope, element, attrs, model) {
+//            var options = {
+//                types: ['(cities)'],
+//                componentRestrictions: {country: 'us'}
+//            };
+//            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+//
+//            google.maps.event.addListener(scope.gPlace, 'place_changed', function () {
+//
+//                var latlongString = this.getPlace().geometry.location.toString();
+//                var cleanlatlongString = latlongString.substring(1, latlongString.length - 1);
+//                var latlonArray = cleanlatlongString.split(',');
+//
+//                scope.userLocationObject.manualCitySearch.lat = latlonArray[0];
+//                scope.userLocationObject.manualCitySearch.lon = latlonArray[1].substring(1);
+//                scope.userLocationObject.manualCitySearch.cityCommaState = this.getPlace().formatted_address;
+//
+//                scope.$apply(function () {
+//                    model.$setViewValue(element.val());
+//                });
+//
+//                scope.awesomeBarSubmit();
+//            });
+//        }
+//    };
+//});
