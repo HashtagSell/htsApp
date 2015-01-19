321ad010881c46ddb4cce6b1d7c3a148e8adfd6b
Brad Davis <bradrobertdavis@gmail.com>
2015-01-18 23:00:18 -0800

    Filter bar is now a seperate module that relies on factory so that we can responsively move filterbar.  more to come.

--- app/js/filterBar/controllers/filterBar.js
+++ app/js/filterBar/controllers/filterBar.js
@@ -1,9 +1,46 @@
-htsApp.controller('filterBar', function ($scope) {
+htsApp.controller('filterBar', ['$scope', '$rootScope', 'searchFactory', function ($scope, $rootScope, searchFactory) {
 
-    $scope.controls = {
-        slider: false,
-        toggleFree: true,
-        togglePhotos: false,
-        toggleMap: false
+    //Any time the user moves to a different page this function is called.
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        if (toState.name === 'results') {
+            $scope.showFilterBar = true;
+        } else {
+            $scope.showFilterBar = false;
+        }
+    });
+
+    //Filtering
+    $scope.filterToggles = searchFactory.filter;
+
+    $scope.$watchGroup(['filterToggles.mustHaveImage', 'filterToggles.mustHavePrice'], function (newValues, oldValues, scope) {
+        //searchFactory.imageFilter(value);
+
+        console.log($scope.filterToggles);
+
+        searchFactory.filterArray();
+
+    });
+
+
+    $scope.rangeSlider = {
+        min: 0,
+        max: 100,
+        step: 1,
+        rangeValue : [2,20]
     };
-});
+
+    $scope.slideDelegate = function (value) {
+        console.log(value);
+    };
+
+    //Adds $ before the price slider values
+    $scope.myFormater = function(value) {
+        return "$"+value;
+    };
+
+    //View
+    $scope.view = searchFactory.view;
+
+    //Tracks state of map visible or not
+    $scope.showMap = 0;
+}]);
