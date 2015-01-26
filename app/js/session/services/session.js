9e2bc955209308d18986561fbe7e0cfe9aeacaa2
Brad Davis <bradrobertdavis@gmail.com>
2015-01-25 23:37:55 -0800

    creating/destroying socket.io rooms when users login/logout or refresh app.

--- app/js/session/services/session.js
+++ app/js/session/services/session.js
@@ -1,34 +1,43 @@
 //This service is getter and setter for user settings, favorites, logged in status etc.
-htsApp.service('Session', ['$window', '$http', '$q', function ($window, $http, $q) {
+htsApp.service('Session', ['$window', '$http', '$q', '$state', 'socketio', function ($window, $http, $q, $state, socketio) {
+
+    this.defaultUserObj = {
+        loggedIn: false,
+        profile_photo: '/images/userMenu/user-placeholder.png',
+        banner_photo: '/images/userMenu/header-placeholder.gif',
+        email_provider: [
+            {
+                name : "Always Ask",
+                value: "ask"
+            }
+        ],
+        favorites: [],
+        feed_categories:[
+            {
+                "name" : "Real Estate",
+                "code" : "RRRR"
+            },
+            {
+                "name" : "For Sale",
+                "code" : "SSSS"
+            },
+            {
+                "name" : "Vehicles",
+                "code" : "VVVV"
+            }
+        ]
+    };
 
     this.userObj = {
-        user_settings : JSON.parse($window.localStorage.getItem("hts_storage")) || {
-            loggedIn: false,
-            profile_photo: '/images/userMenu/user-placeholder.png',
-            banner_photo: '/images/userMenu/header-placeholder.gif',
-            email_provider: [
-                {
-                    name : "Always Ask",
-                    value: "ask"
-                }
-            ],
-            favorites: [],
-            feed_categories:[
-                {
-                    "name" : "Real Estate",
-                    "code" : "RRRR"
-                },
-                {
-                    "name" : "For Sale",
-                    "code" : "SSSS"
-                },
-                {
-                    "name" : "Vehicles",
-                    "code" : "VVVV"
-                }
-            ]
-        }
+        user_settings : JSON.parse($window.localStorage.getItem("hts_storage")) || this.defaultUserObj
     };
+
+    //if the user refreshes the app or opens app back up and user is still logged in then we join all their rooms.
+    if($window.localStorage.hts_storage){
+        socketio.username = this.userObj.user_settings.name;
+        socketio.init();
+    }
+
 
     //Call this function when user updates HTML5 Session Storage.  Keeps server in sync.
     this.updateServer = function (callback) {
@@ -68,13 +77,29 @@ htsApp.service('Session', ['$window', '$http', '$q', function ($window, $http, $
         data.user_settings.loggedIn = true;
         this.userObj.user_settings = data.user_settings; //ONLY ADD USER_SETTING PROPERTY TO OBJECT OTHERWISE BINDING FAILS AND UI DOES NOT LIVE UPDATE.
         console.log('data about to be written to local storage', this.userObj);
+
+        if(!$window.localStorage.hts_storage){
+            console.log('this is first login. Init socket.io to all rooms of items they are selling');
+            // init to tell server who client is
+            socketio.username = this.userObj.user_settings.name;
+            socketio.init();
+        }
+
         $window.localStorage.hts_storage = angular.toJson(this.userObj.user_settings);
     };
 
     //Clears all users settings from HTML5 session storage on logout
     this.destroy = function () {
+        console.log('clearing user obj');
+        this.userObj.user_settings = this.defaultUserObj;
+
+        console.log('destroying all socket.io connections');
+        socketio.closeAllConnections();
+
         console.log("Destroying HTML5 Session");
         $window.localStorage.removeItem("hts_storage");
+
+        $state.go('feed');
     };
 
     //Get a particular user setting from HTML5 session storage
