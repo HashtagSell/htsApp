57a83bf3caad16d6724ea29e61169db2a473e860
Brad Davis <bradrobertdavis@gmail.com>
2015-01-27 14:21:05 -0800

    fixes #9

--- app/js/sideProfile/controllers/sideProfile.controller.js
+++ app/js/sideProfile/controllers/sideProfile.controller.js
@@ -3,14 +3,3 @@ htsApp.controller('sideProfile', ['$scope', 'Session', function ($scope, Session
     $scope.userObj = Session.userObj;
 
 }]);
-
-
-htsApp.directive('backImg', function () {
-    return function (scope, element, attrs) {
-        var url = attrs.backImg;
-        element.css({
-            'background-image' : "url(" + url + ")",
-            'background-size' : "cover"
-        });
-    };
-});
