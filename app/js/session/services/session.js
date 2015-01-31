c70b82d505e13c1a55786c6b75f0e2be7570b8eb
Brad Davis <bradrobertdavis@gmail.com>
2015-01-31 15:17:20 -0800

    code cleanup

--- app/js/session/services/session.js
+++ app/js/session/services/session.js
@@ -29,11 +29,24 @@ htsApp.service('Session', ['$window', '$http', '$q', '$state', 'socketio', funct
         ]
     };
 
+
     this.userObj = {
         user_settings : JSON.parse($window.localStorage.getItem("hts_storage")) || this.defaultUserObj
     };
 
     //if the user refreshes the app or opens app back up and user is still logged in then we join all their rooms.
+    //if($window.localStorage.hts_storage){
+    //    this.userObj = {
+    //        user_settings : JSON.parse($window.localStorage.getItem("hts_storage"))
+    //    };
+    //    socketio.username = this.userObj.user_settings.name;
+    //    socketio.init();
+    //} else {
+    //    this.userObj = {
+    //        user_settings : this.defaultUserObj
+    //    };
+    //}
+
     if($window.localStorage.hts_storage){
         socketio.username = this.userObj.user_settings.name;
         socketio.init();
