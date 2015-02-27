769ba32df484825b7d23097f53cc8baa1f8abe98
Brad Davis <bradrobertdavis@gmail.com>
2015-02-26 22:22:21 -0800

    Adopting all new socket.io namespaces introduced with new realtime api.

--- app/js/socketio/factories/socketio.factory.js
+++ app/js/socketio/factories/socketio.factory.js
@@ -1,145 +1,149 @@
 /**
  * Created by braddavis on 1/25/15.
  */
-htsApp.factory('socketio', ['sellingFactory', function (sellingFactory) {
+htsApp.factory('socketio', ['sellingFactory', 'ENV', '$http', '$q', 'Session', 'offersFactory', 'qaFactory', 'messagesFactory', function (sellingFactory, ENV, $http, $q, Session, offersFactory, qaFactory, messagesFactory) {
 
     var socketio = {
-        socket: io('https://stage-realtime.hashtagsell.com/postings'),
+        postingSocket: io(ENV.realtimePostingAPI),
+        userSocket: io(ENV.realtimeUserAPI),
         postings: [],
-        username: 'anon'
+        usersViewing: []
     };
 
 
 
-
-
-    socketio.init = function () {
-        sellingFactory.init().then(function (response) {
-            if (response.status === 200) {
-
-                response.data.forEach(function (posting) {
-                    socketio.joinRoom(posting.external_id);
-                });
-            }
-
-        }, function (response) {
-
-            console.log(response);
-
-            //TODO: Use modal service to notify users
-            console.log("socket.io could not lookup items for sale");
-        });
-    };
-
-    socketio.joinRoom = function (postingId) {
+    socketio.joinPostingRoom = function (postingId) {
         socketio.postings.push(postingId);
 
-        console.log('joining room: ' + postingId);
+        console.log(Session.userObj.user_settings.name + ' is joining posting room: ' + postingId);
 
-        socketio.socket.emit('join-room', {
-            username : socketio.username,
+        socketio.postingSocket.emit('join-room', {
+            username : Session.userObj.user_settings.name,
             roomId : postingId
         });
     };
 
-    // capture any hiccups in the connection and re-init as needed
-    socketio.socket.on('reconnect', socketio.init);
 
 
+    socketio.joinUserRoom = function (ownerUsername, visitor) {
+        socketio.usersViewing.push(visitor);
 
+        console.log(visitor + ' is joining ' + ownerUsername + '\'s room: ');
+
+        socketio.userSocket.emit('join-room', {
+            username : visitor,
+            roomId : ownerUsername
+        });
+    };
 
 
 
     //Leave all rooms when user logs out.
     socketio.closeAllConnections = function () {
-        sellingFactory.init().then(function (response) {
-            if (response.status === 200) {
 
-                response.data.forEach(function (posting) {
-                    socketio.leaveRoom(posting.external_id);
-                });
-            }
+        if(sellingFactory.itemsForSale) {
+            sellingFactory.itemsForSale.forEach(function (posting) {
+                socketio.leavePostingRoom(posting.postingId);
+            });
+        }
 
-        }, function (response) {
 
-            console.log(response);
+        socketio.leaveUserRoom(socketio.cachedUsername);
 
-            //TODO: Use modal service to notify users
-            console.log("socket.io could not lookup items for sale");
-        });
+        socketio.cachedUsername = null;
     };
 
 
-    socketio.leaveRoom = function (postingId) {
+    socketio.leavePostingRoom = function (postingId) {
 
-        console.log('leaving room: ' + postingId);
+        console.log('leaving posting room: ' + postingId);
 
         socketio.postings.splice(socketio.postings.indexOf(postingId), 1);
 
-        socketio.socket.emit('leave-room', postingId);
+        socketio.postingSocket.emit('leave-room', postingId);
+    };
+
+
+    socketio.leaveUserRoom = function (username) {
+
+        socketio.usersViewing.splice(socketio.usersViewing.indexOf(username), 1);
+
+        console.log('leaving user room: ' + username);
+
+        socketio.userSocket.emit('leave-room', username);
     };
 
 
 
 
-
-
-    socketio.sendMessage = function (postingId, messageText) {
-        socketio.socket.emit('message', {
+    socketio.sendMessage = function (recipient, messageText) {
+        socketio.userSocket.emit('private-message', {
             message : messageText,
-            roomId : postingId
+            recipient: recipient,
+            username : Session.userObj.user_settings.name
         });
     };
-
-    socketio.updates = {
-        messages: []
-    };
-
-
-    socketio.renderMessage = function (message) {
-        console.log(
-            '%s said "%s" at %s in room %s',
-            message.username,
-            message.message,
-            message.timestamp,
-            message.roomId
-        );
-        socketio.updates.messages.unshift(message);
-        console.log('socketfactory sees: ', socketio.updates);
-    };
-
 
 
 
     // listen for messages
-    socketio.socket.on('message', socketio.renderMessage);
+    socketio.userSocket.on('private-message', messagesFactory.newMessageNotification);
+
+
+    // listen for offers
+    socketio.postingSocket.on('make-offer', function (offer) {
+        //TODO: Need the offer object to include the sellers username
+        if (Session.userObj.user_settings.name !== offer.username) {
+            offersFactory.newOfferNotification(offer);
+        }
+    });
+
+
+    // listen for questions
+    socketio.postingSocket.on('question', function (question) {
+        //TODO: Need the questions object to include the sellers username
+        if (Session.userObj.user_settings.name !== question.username) {
+            qaFactory.notifySellerOfNewQuestion(question);
+        }
+    });
+
+
+    // listen for answers
+    socketio.postingSocket.on('answer', function (answer) {
+        console.log(answer);
+    });
+
+
+    // capture any hiccups in the connection and re-init as needed
+    socketio.userSocket.on('reconnect', socketio.init);
+    socketio.postingSocket.on('reconnect', socketio.init);
 
 
 
+    socketio.init = function () {
+        if(Session.userObj.user_settings.loggedIn) {
+            socketio.cachedUsername = Session.userObj.user_settings.name;
+
+            sellingFactory.getUsersItemsForSale(Session.userObj.user_settings.name).then(function (response) {
+                if (response.status === 200) {
+
+                    response.data.results.forEach(function (posting) {
+                        socketio.joinPostingRoom(posting.postingId);
+                    });
+                }
+
+            }, function (response) {
+
+                console.log(response);
+
+                //TODO: Use modal service to notify users
+                console.log("socket.io could not lookup items for sale");
+            });
 
 
-
-    //socketio.askSellerQuestion = function (postingId, questionText) {
-    //    socketio.socket.emit('askSellerQuestion', {
-    //        question : questionText,
-    //        roomId : postingId
-    //    });
-    //};
-    //
-    //
-    //socketio.renderBuyerQuestion = function (question) {
-    //    console.log(
-    //        '%s asked "%s" at %s in room %s',
-    //        question.username,
-    //        question.message,
-    //        question.timestamp,
-    //        question.roomId
-    //    );
-    //};
-    //
-    //// listen for messages
-    //socketio.socket.on('askSellerQuestion', socketio.renderBuyerQuestion);
-
+            socketio.joinUserRoom(Session.userObj.user_settings.name, Session.userObj.user_settings.name);
+        }
+    };
 
 
     return socketio;
