5f35169a3e9cd3152a3b1b48c2069ce9ccf8b4d8
Brad Davis <bradrobertdavis@gmail.com>
2015-02-26 22:08:02 -0800

    Added logic to start freegeoip service if crashed or not already started.. happens sometimes.

--- utils/ipgeolocation.js
+++ utils/ipgeolocation.js
@@ -1,6 +1,8 @@
 var request = require("request");
 var common   = require('../config/common.js');
 var config   = common.config();
+var sys = require('sys');
+var exec = require('child_process').exec;
 
 exports.lookup = function(ip, promise){
 
@@ -16,16 +18,16 @@ exports.lookup = function(ip, promise){
         return false;
     };
 
-    if(isPrivateOrLocalIP(ip)){
+    if(isPrivateOrLocalIP(ip)){ //Hardcode the IP if we are running on location machine.
         ip = "216.38.134.18";
         //ip = "104.156.240.172";
     }
 
-    var freeGeoIp = config.FREEGEOIP_URL + ip;
+    var freeGeoIp = config.hts.geolocation_api.url + ip;
 
     request(freeGeoIp, function (error, response, body) {
 
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
 
             console.log(body);
 
@@ -33,13 +35,32 @@ exports.lookup = function(ip, promise){
 
         } else if (error) {
 
-            console.log(error);
+            console.log(error.code);
 
-            promise(error, null);
+            if (error.code === "ECONNREFUSED") {
+
+
+                console.log("*** FreeGeoIP Crashed!!");
+                console.log("*** Starting FreeGeoIP again.");
+
+
+
+                function puts(error, stdout, stderr) {
+                    sys.puts(stdout);
+                };
+
+                exec("./startFreeGeoIpIfNotRunning.sh", puts);
+
+                console.log('*** Waiting for 3 seconds.');
+
+                setTimeout(function() {
+                    console.log("*** Attempting Geolocation again");
+                    exports.lookup(ip, promise);
+                }, 3000);
+
+            }
 
         } else {
-
-            console.log("Fell into else.");
 
             console.log(error, response, body);
         }
