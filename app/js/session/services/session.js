d3f96a379cbd4ba36b998c2bd552391534119d4f
Brad Davis <bradrobertdavis@gmail.com>
2015-02-26 22:18:58 -0800

    removing socket.io dependency inside session.js.  Was causing circular dependency errors in other factories.

--- app/js/session/services/session.js
+++ app/js/session/services/session.js
@@ -1,5 +1,5 @@
 //This service is getter and setter for user settings, favorites, logged in status etc.
-htsApp.service('Session', ['$window', '$http', '$q', '$state', 'socketio', function ($window, $http, $q, $state, socketio) {
+htsApp.service('Session', ['$window', '$http', '$q', '$state', function ($window, $http, $q, $state) {
 
     this.defaultUserObj = {
         loggedIn: false,
@@ -33,24 +33,6 @@ htsApp.service('Session', ['$window', '$http', '$q', '$state', 'socketio', funct
     this.userObj = {
         user_settings : JSON.parse($window.localStorage.getItem("hts_storage")) || this.defaultUserObj
     };
-
-    //if the user refreshes the app or opens app back up and user is still logged in then we join all their rooms.
-    //if($window.localStorage.hts_storage){
-    //    this.userObj = {
-    //        user_settings : JSON.parse($window.localStorage.getItem("hts_storage"))
-    //    };
-    //    socketio.username = this.userObj.user_settings.name;
-    //    socketio.init();
-    //} else {
-    //    this.userObj = {
-    //        user_settings : this.defaultUserObj
-    //    };
-    //}
-
-    if($window.localStorage.hts_storage){
-        socketio.username = this.userObj.user_settings.name;
-        socketio.init();
-    }
 
 
     //Call this function when user updates HTML5 Session Storage.  Keeps server in sync.
@@ -92,13 +74,6 @@ htsApp.service('Session', ['$window', '$http', '$q', '$state', 'socketio', funct
         this.userObj.user_settings = data.user_settings; //ONLY ADD USER_SETTING PROPERTY TO OBJECT OTHERWISE BINDING FAILS AND UI DOES NOT LIVE UPDATE.
         console.log('data about to be written to local storage', this.userObj);
 
-        if(!$window.localStorage.hts_storage){
-            console.log('this is first login. Init socket.io to all rooms of items they are selling');
-            // init to tell server who client is
-            socketio.username = this.userObj.user_settings.name;
-            socketio.init();
-        }
-
         $window.localStorage.hts_storage = angular.toJson(this.userObj.user_settings);
     };
 
@@ -106,9 +81,6 @@ htsApp.service('Session', ['$window', '$http', '$q', '$state', 'socketio', funct
     this.destroy = function () {
         console.log('clearing user obj');
         this.userObj.user_settings = this.defaultUserObj;
-
-        console.log('destroying all socket.io connections');
-        socketio.closeAllConnections();
 
         console.log("Destroying HTML5 Session");
         $window.localStorage.removeItem("hts_storage");
